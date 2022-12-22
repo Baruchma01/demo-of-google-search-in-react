a7b127938f3e54a6061c6fa0d4c892a49404bef3
Baruch Mashasha <baruchma01@gmail.com>
2022-12-22 12:28:57 +0200

    filter logic

--- google-search/src/components/Search/Search.js
+++ google-search/src/components/Search/Search.js
@@ -1,5 +1,32 @@
 import { Input } from "./Input/Input";
+import { useState, useEffect } from "react";
 
 export const Search = () => {
-  return <Input />;
+  const [data, setData] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+
+  // search items from the array
+  const handleSearch = (query) => {
+    if (query.length > 0) {
+      const filteredItems = data?.filter((item) =>
+        item.title.toLowerCase().includes(query)
+      );
+      setFilteredItems(filteredItems);
+    } else {
+      setFilteredItems([]);
+    }
+  };
+
+  // fetch data from the api and save the data in state
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://dummyjson.com/products");
+      const data = await response.json();
+      const { products } = data;
+      setData(products);
+    }
+    fetchData();
+  }, []);
+
+  return <Input handleSearch={handleSearch} />;
 };
