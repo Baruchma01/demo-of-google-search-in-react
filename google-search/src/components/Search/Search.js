29e26570e0076a0165a2db97c7f34e867f26660c
Baruch Mashasha <baruchma01@gmail.com>
2022-12-25 12:47:58 +0200

    logic for keyboard

--- google-search/src/components/Search/Search.js
+++ google-search/src/components/Search/Search.js
@@ -1,12 +1,32 @@
 import { Input } from "./Input/Input";
 import { useState, useEffect } from "react";
 import { Items } from "./items/Items";
+import { useNavigate } from "react-router-dom";
 
 export const Search = () => {
+  const navigate = useNavigate();
   const [data, setData] = useState([]);
   const [filteredItems, setFilteredItems] = useState([]);
+  const [cursor, setCursor] = useState(0);
 
-  // search items from the array
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 38 && cursor > 0) {
+      setCursor(cursor - 1);
+    } else if (e.keyCode === 40 && cursor < filteredItems.length - 1) {
+      setCursor(cursor + 1);
+    } else if (e.keyCode === 13) {
+      const item = filteredItems[cursor];
+      if (item) {
+        handleClick(item);
+      }
+    }
+  };
+
+  const handleClick = (item) => {
+    const query = `q=${item.id}`;
+    navigate(`/search?${query}`);
+  };
+
   const handleSearch = (query) => {
     if (query.length > 0) {
       const filteredItems = data?.filter((item) =>
@@ -18,7 +38,6 @@ export const Search = () => {
     }
   };
 
-  // fetch data from the api and save the data in state
   useEffect(() => {
     async function fetchData() {
       const response = await fetch("https://dummyjson.com/products");
@@ -30,9 +49,14 @@ export const Search = () => {
   }, []);
 
   return (
-    <div className="search-container" style={{ height: "500px" }}>
-      <Input handleSearch={handleSearch} />
-      <Items items={filteredItems} />
+    <div className="search-container">
+      <Input handleSearch={handleSearch} handleKeyDown={handleKeyDown} />
+      <Items
+        items={filteredItems}
+        cursor={cursor}
+        handleClick={(item) => handleClick(item)}
+        handleMouseEnter={(i) => setCursor(i)}
+      />
     </div>
   );
 };
